d7bea58c243ae0a903e0e8b185b3cba22496aab3
J. Ruben Daza Hernandez <rudahee@gmail.com>
2024-01-19 23:16:48 +0100

    added config

--- src/main/java/net/steelcode/discord/config/Configuration.java
+++
@@ -1,8 +0,0 @@
-package net.steelcode.discord.config;
-
-public class Configuration {
-
-    public static final String TOKEN = "MTE1MTA3NjQ1MzAyNzQyNjM1NA.G6YAaC.Zxmdxc4K5C0Pe2fkr5kYWp_ECjUOXkHAtteqRo";
-
-    public static final String KEYPASS = "1NA.G6YAaC.Zxmd";
-}
